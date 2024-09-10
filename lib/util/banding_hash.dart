7bb89f65817cacbda1d6ccf137597ddc2da9dcc5
zaiehilmi <zaiehilmi@gmail.com>
2024-09-10 12:34:37 +0800

    docs: tambah dokumentasi

--- lib/util/banding_hash.dart
+++ lib/util/banding_hash.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:crypto/crypto.dart';
 
+@Deprecated('tak pakai dalam code')
 bool bandingHash(String failDimuatturun, String dariAPi) {
   List<int> local = File(failDimuatturun).readAsBytesSync();
   final localDigest = md5.convert(local).toString();
--- lib/util/roggle.dart
+++ lib/util/roggle.dart
@@ -1,3 +1,4 @@
 import 'package:roggle/roggle.dart';
 
+/// gunakan rog untuk logging
 final rog = Roggle();
