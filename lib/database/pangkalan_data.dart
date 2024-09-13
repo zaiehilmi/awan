255448972c73703b17c2f50f3816f451e508162e
zaiehilmi <zaiehilmi@gmail.com>
2024-09-13 22:38:00 +0800

    chore: tambah logStatement utk dapatkan query yang dihantar

--- lib/database/pangkalan_data.dart
+++ lib/database/pangkalan_data.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:drift/drift.dart';
-import 'package:drift_flutter/drift_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'package:drift/native.dart';
 
 import '../model/gtfs/index.dart';
 import 'dao/laluan_bas.dart';
@@ -23,8 +27,13 @@ class PangkalanDataApl extends _$PangkalanDataApl {
 
   @override
   int get schemaVersion => 1;
+}
 
-  static QueryExecutor _openConnection() {
-    return driftDatabase(name: 'my_database');
-  }
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+
+    return NativeDatabase.createInBackground(file, logStatements: true);
+  });
 }
